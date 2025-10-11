95ddaeb20da7080f6cde06f5e9546c77d65d2379
Shuvam Mitra <shuvammitra05@gmail.com>
2025-10-11 23:23:39 +0530

    ga testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sem from "./pages/sem";
 import Cg from "./pages/cg";
 import Home from "./pages/home";
+import ReactGA from "react-ga4";
+import useAnalytics from "./hooks/useAnalytics";
+
+// ReactGA.initialize("G-84SL8R8GP4");
+// ReactGA.send("pageview");
 
 function App() {
+  // useAnalytics();
+
+  useEffect(() => {
+        ReactGA.initialize("G-84SL8R8GP4");
+        ReactGA.send({ hitType: "pageview", page: "/lmao", title: "Landing Page", debug_mode: true });
+    }, [])
+
   return (
     <BrowserRouter>
       <Routes>
